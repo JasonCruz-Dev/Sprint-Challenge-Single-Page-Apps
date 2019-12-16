1e8a51442e7cdffa754109d74c84c75a2fe6595a
Jason Cruz <badmonkey.dev@gmail.com>
2019-12-15 20:29:05 -0600

    imported navlink, searchform, welcomepage, characterlist

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 import Header from "./components/Header.js";
-
+import { Route, NavLink } from "react-router-dom";
+import SearchForm from "./components/SearchForm";
+import WelcomePage from "./components/WelcomePage";
+import CharacterList from "./components/CharacterList";
+import SearchForm from "./components/SearchForm";
 
 export default function App() {
   return (
