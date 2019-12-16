0a10c79a5227155e4d88ef8c4357a26acb8bfb07
Jason Cruz <badmonkey.dev@gmail.com>
2019-12-15 20:58:23 -0600

    added search functionality, need to add filter to complete it, will come back to that

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,45 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function SearchForm() {
- 
+  const [value, setValue] = useState([]);
+  const [foundValue, setFoundValue] = useState([]);
+
+  const searchOnChange = e => {
+    setValue(e.target.value);
+  };
+
+  const onSearch = e => {
+    e.preventDefault();
+    const name = value;
+
+    axios
+      .get(`https://rickandmortyapi.com/api/character/?name=${name}`)
+      .then(res => {
+        console.log(res.data.results);
+        setFoundValue(res.data.results);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   return (
-    <section className="search-form">
-     // Add a search form here
-    </section>
+    <>
+      <form onSubmit={onSearch}>
+        <input
+          className='search-form'
+          placeholder='Search by First or Last Name'
+          onChange={searchOnChange}
+        />
+        <button type='submit'>Search</button>
+      </form>
+      {foundValue.map(res => (
+        <div>{res.name}</div>
+      ))}
+    </>
+    // {/* <section className="search-form">
+    //  // Add a search form here
+    // </section> */}
   );
 }
