585e262b0a5aa585c921f37abfd9b3c0fb72f5f6
Jason Cruz <badmonkey.dev@gmail.com>
2019-12-15 20:37:11 -0600

    added navigation links to app.js file and added some components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Header from "./components/Header.js";
 import { Route, NavLink } from "react-router-dom";
-import SearchForm from "./components/SearchForm";
 import WelcomePage from "./components/WelcomePage";
 import CharacterList from "./components/CharacterList";
 import SearchForm from "./components/SearchForm";
@@ -10,6 +9,23 @@ export default function App() {
   return (
     <main>
       <Header />
+      <NavLink to='/'>
+        <p>Home</p>
+      </NavLink>
+      <NavLink to='/characters'>
+        <p>Characters</p>
+      </NavLink>
+      <NavLink to='/location'>
+        <p>Location</p>
+      </NavLink>
+      <NavLink to='episode'>
+        <p>Episode</p>
+      </NavLink>
+
+      <SearchForm />
+
+      <Route exact path='/' component={WelcomePage} />
+      <Route path='/characters' component={CharacterList} />
     </main>
   );
 }
